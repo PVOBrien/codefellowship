83a6436de88c51ac9a75feffc1633ed992202bc5
Paul OBrien <PVOVideo@outlook.com>
2020-10-05 20:04:17 -0700

    login successful. route after that: not.

---
+++ src/main/java/com/example/codefellowship/models/user/ApplicationUser.java
@@ -0,0 +1,84 @@
+package com.example.codefellowship.models.user;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Collection;
+
+@Entity
+public class ApplicationUser implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+
+    String username; // is potato
+    String password; // is potato
+
+    String firstName;
+    String lastName;
+    long dateOfBirth;
+    String bio;
+
+    public ApplicationUser(String userName, String password, String firstName, String lastName, String bio){}
+
+    public ApplicationUser(String username, String password, String firstName, String lastName, long dateOfBirth, String bio) {
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.bio = bio;
+    }
+
+    public long getId() { return id; }
+    public void setId(long id) { this.id = id; }
+
+    public String getUsername() { return username; }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+
+    public void setUsername(String username) { this.username = username; }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+
+    public long getDateOfBirth() { return dateOfBirth; }
+    public void setDateOfBirth(long dateOfBirth) { this.dateOfBirth = dateOfBirth; }
+
+    public String getBio() { return bio; }
+    public void setBio(String bio) { this.bio = bio; }
+}
